832a159db65574c781fde4779e7f57ff57ad9f10
nedrazzz <smg9972@gmail.com>
2025-03-05 13:31:56 +0900

    chore: layout 변경경

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -7,16 +7,16 @@ interface Tag {
 }
 
 interface File {
-  id: number;
-  name: string;
-  url: string;
+  extension: string;
+  downloadUrl: string;
 }
 
 interface Document {
   id: number;
   title: string;
+  description: string;
   tags: Tag[];
-  files: File[];
+  file: File;
 }
 
 const ArchivePage = () => {
@@ -26,28 +26,55 @@ const ArchivePage = () => {
     { id: 3, name: "태그3" },
     { id: 4, name: "태그4" },
     { id: 5, name: "태그5" },
+    { id: 6, name: "태그6" },
+    { id: 7, name: "태그7" },
+    { id: 8, name: "태그8" },
+    { id: 9, name: "태그9" },
   ];
 
-  const files: File[] = [
-    { id: 1, name: "file1", url: "#" },
-    { id: 2, name: "file2", url: "#" },
-    { id: 3, name: "file3", url: "#" },
-    { id: 4, name: "file4", url: "#" },
-    { id: 5, name: "file5", url: "#" },
-  ];
+  const file: File = {
+    extension: "HWP",
+    downloadUrl: "#",
+  };
 
   // Mock data for resources
   const docuemnts: Document[] = [
-    { id: 1, title: "자료 항목 1", tags: [tags[1], tags[2], tags[3]], files: [files[1], files[2]] },
-    { id: 2, title: "자료 항목 2", tags: [tags[2], tags[3]], files: [files[2]] },
-    { id: 3, title: "자료 항목 3", tags: [tags[4]], files: [files[1], files[3], files[4]] },
-    { id: 4, title: "자료 항목 4", tags: [tags[4]], files: [files[1], files[3], files[4]] },
-    { id: 5, title: "자료 항목 5", tags: [tags[4]], files: [files[1], files[3], files[4]] },
-    { id: 6, title: "자료 항목 6", tags: [tags[4]], files: [files[1], files[3], files[4]] },
+    {
+      id: 1,
+      title: "자료 항목 1",
+      description:
+        "Ex do adipisicing ut dolore qui. Proident qui voluptate velit sit sunt sint deserunt officia id laboris consectetur. Aliqua dolore sunt do exercitation adipisicing pariatur irure cillum dolor irure ad. Minim ea enim elit tempor laborum qui quis enim proident eiusmod anim elit laboris id.",
+      tags: [tags[1], tags[2], tags[3]],
+      file: file,
+    },
+    {
+      id: 2,
+      title: "자료 항목 2",
+      description:
+        "Ex do adipisicing ut dolore qui. Proident qui voluptate velit sit sunt sint deserunt officia id laboris consectetur. Aliqua dolore sunt do exercitation adipisicing pariatur irure cillum dolor irure ad. Minim ea enim elit tempor laborum qui quis enim proident eiusmod anim elit laboris id.",
+      tags: [],
+      file: file,
+    },
+    {
+      id: 3,
+      title: "자료 항목 3",
+      description:
+        "Ex do adipisicing ut dolore qui. Proident qui voluptate velit sit sunt sint deserunt officia id laboris consectetur. Aliqua dolore sunt do exercitation adipisicing pariatur irure cillum dolor irure ad. Minim ea enim elit tempor laborum qui quis enim proident eiusmod anim elit laboris id.",
+      tags: [tags[0], tags[1], tags[4]],
+      file: file,
+    },
+    {
+      id: 4,
+      title: "자료 항목 4",
+      description:
+        "Ex do adipisicing ut dolore qui. Proident qui voluptate velit sit sunt sint deserunt officia id laboris consectetur. Aliqua dolore sunt do exercitation adipisicing pariatur irure cillum dolor irure ad. Minim ea enim elit tempor laborum qui quis enim proident eiusmod anim elit laboris id.",
+      tags: [tags[0], tags[1], tags[4]],
+      file: file,
+    },
   ];
 
   return (
-    <main className="mx-4">
+    <div className="mx-4">
       <header className="mb-4">
         <h1 className="font-bold text-xl">자료실</h1>
       </header>
@@ -57,16 +84,20 @@ const ArchivePage = () => {
           <input
             type="text"
             placeholder="제목으로 검색하세요"
-            className="w-full px-10 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-300 transition"
+            className="overflow-x-auto whitespace-nowrap w-full px-10 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-300 transition"
           />
           <MdManageSearch className="absolute left-3 top-3 text-gray-400" size={20} />
         </div>
       </section>
       {/* Tags */}
-      <section className="mb-4 flex gap-2">
-        {tags.map((tag) => (
-          <TagItem key={tag.id} tag={tag} />
-        ))}
+      <section className="mb-4 overflow-x-auto">
+        <ul className="flex gap-2">
+          {tags.map((tag) => (
+            <li key={tag.id}>
+              <TagItem tag={tag} />
+            </li>
+          ))}
+        </ul>
       </section>
       {/* Document */}
       <section className="mb-4 flex flex-col gap-2">
@@ -74,14 +105,14 @@ const ArchivePage = () => {
           <DocumentItem key={document.id} document={document} />
         ))}
       </section>
-    </main>
+    </div>
   );
 };
 
 const TagItem = ({ tag }: { tag: Tag }) => {
   return (
     <button
-      className={`px-2 py-1 rounded-full border transition bg-white hover:bg-gray-50 text-xs`}>
+      className={`flex-none min-w-16 px-2 py-1 rounded-full border bg-white hover:bg-gray-50 text-sm`}>
       {tag.name}
     </button>
   );
@@ -91,21 +122,23 @@ const DocumentItem = ({ document }: { document: Document }) => {
   return (
     <article className="flex justify-center items-center w-full">
       <div className="border-2 rounded-2xl border-black w-full px-4 py-4 relative">
-        <div className="flex flex-col gap-1">
+        <section className="flex flex-col gap-1">
           <header className="font-bold text-xl">{document.title}</header>
           <ul className="flex gap-2 text-sm">
+            {document.tags.length === 0 ? <li>태그 없음</li> : <></>}
             {document.tags.map((tag) => (
               <li key={tag.id}>#{tag.name}</li>
             ))}
           </ul>
-          <ul className="flex gap-2 text-sm">
-            {document.files.map((file) => (
-              <li key={file.id}>
-                <a href={file.url}>{file.name}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
+          <p className="text-sm line-clamp-2">{document.description}</p>
+        </section>
+        <section className="absolute top-4 right-4">
+          <a
+            href={document.file.extension}
+            className="border-2 border-black rounded-full w-12 h-12 flex items-center justify-center">
+            <span className="font-bold text-sm">{document.file.extension}</span>
+          </a>
+        </section>
       </div>
     </article>
   );
