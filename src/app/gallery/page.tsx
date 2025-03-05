35553f01c20027f26492d1259543de82718e3bf9
nedrazzz <smg9972@gmail.com>
2025-03-05 15:35:32 +0900

    feat: 갤러리 사진 상세보기 구현현

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { IoCamera } from "react-icons/io5";
 import { TbLoader2 } from "react-icons/tb";
+import { IoIosCloseCircle } from "react-icons/io";
 
 const GalleryPage = () => {
   return (
@@ -44,10 +45,12 @@ const Gallery = () => {
         { length: 16 },
         (_, i): Photo => ({
           id: i + 1,
-          src: `/test1.png`,
+          src: `/test3.png`,
           alt: `사진 ${i + 1}`,
           title: `사진 제목 ${i + 1}`,
-          description: `사진 ${i + 1}에 대한 설명입니다.`,
+          description: `사진 ${
+            i + 1
+          } : Ad sit id cillum incididunt ullamco laborum amet ipsum dolor veniam. Aute consequat adipisicing ad irure nostrud consectetur veniam irure et elit et. Nisi ut elit sunt quis magna mollit sit.`,
           date: new Date(2024, i % 12, (i % 28) + 1),
         })
       );
@@ -59,22 +62,52 @@ const Gallery = () => {
     fetchPhotos();
   }, []);
 
-  return <div>{loading ? <PhotoGridLoading /> : <PhotoGrid photos={photos} />}</div>;
-};
+  const [seletedPhoto, setSelectedPhoto] = useState<Photo | null>(null);
+  const openDetail = (photo: Photo) => {
+    setSelectedPhoto(photo);
+  };
+  const closeDetail = () => {
+    setSelectedPhoto(null);
+  };
 
-const PhotoGridLoading = () => {
   return (
-    <div className="flex items-center justify-center h-64">
-      <TbLoader2 className="animate-spin" size={80} color="blue" />
+    <div>
+      {loading ? (
+        <PhotoGridLoading />
+      ) : (
+        <div>
+          <PhotoGrid photos={photos} onClickPhoto={openDetail} />
+          {seletedPhoto ? <PhotoDetail photo={seletedPhoto} onClose={closeDetail} /> : null}
+        </div>
+      )}
     </div>
   );
 };
 
-const PhotoGrid = ({ photos }: { photos: Photo[] }) => {
+const PhotoGridLoading = () => {
   return (
-    <div className="grid grid-cols-3 gap-2">
+    <section className="flex items-center justify-center h-64">
+      <TbLoader2 className="animate-spin" size={80} color="blue" />
+    </section>
+  );
+};
+
+const PhotoGrid = ({
+  photos,
+  onClickPhoto: clickPhoto,
+}: {
+  photos: Photo[];
+  onClickPhoto: (photo: Photo) => void;
+}) => {
+  return (
+    <section className="grid grid-cols-3 gap-2">
       {photos.map((photo) => (
-        <div key={photo.id} className="relative pb-[100%]">
+        <div
+          key={photo.id}
+          className="relative pb-[100%]"
+          onClick={() => {
+            clickPhoto(photo);
+          }}>
           <Image
             src={photo.src}
             alt={photo.alt}
@@ -86,7 +119,37 @@ const PhotoGrid = ({ photos }: { photos: Photo[] }) => {
           />
         </div>
       ))}
-    </div>
+    </section>
+  );
+};
+
+const PhotoDetail = ({ photo, onClose }: { photo: Photo; onClose: () => void }) => {
+  return (
+    <section className="fixed inset-0 z-50 bg-black bg-opacity-85">
+      <div className="w-full h-full flex flex-col md:flex-row items-stretch ">
+        <section className="relative basis-40 md:basis-60 p-4 bg-black bg-opacity-50 text-white ">
+          <h3 className="text-lg font-bold">{photo.title}</h3>
+          <p className="mt-3 text-sm">{photo.description}</p>
+          <p className="text-xs text-gray-300 mt-1">{photo.date.toDateString()}</p>
+          <button
+            className="absolute top-2 right-2 z-10 w-10 h-10"
+            onClick={() => {
+              onClose();
+            }}>
+            <IoIosCloseCircle className="w-full h-full" />
+          </button>
+        </section>
+        <section className="grow relative">
+          <Image
+            src={photo.src}
+            alt={photo.alt}
+            fill
+            quality={100}
+            className="absolute inset-0 object-contain"
+          />
+        </section>
+      </div>
+    </section>
   );
 };
 
